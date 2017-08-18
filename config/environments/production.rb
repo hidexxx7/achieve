79959206c2c5cc902e34baadf74ff2442410427b
小島秀樹 <kojimahideki@MacBook-Pro.local>
2017-08-19 01:10:00 +0900

    DIVE20-2 修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'https://salty-anchorage-23948.herokuapp.com/' }
+  #config.action_mailer.default_url_options = { host: 'https://salty-anchorage-23948.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: '52.197.252.217' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
   {
